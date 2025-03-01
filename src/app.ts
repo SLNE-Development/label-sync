b49310b5221bd5e8d44197de3081f0adc9e2c075
Ammo <ammo@slne.dev>
2025-03-01 01:23:21 +0100

    Update repository logging in app.ts: use conditional repo name for completion message

--- src/app.ts
+++ src/app.ts
@@ -19,9 +19,9 @@ const main = async () => {
 			labels: labels,
 		})
 			.then((_diff: AnyDiff) => {
-				// const repoName = repo.private ? "Private" : repo.full_name;
+				const repoName = repo.private ? "Private" : repo.full_name;
 
-				console.log(`Repo: ${repo.full_name} done!`);
+				console.log(`Repo: ${repoName} done!`);
 			})
 			.catch((error: any) => {
 				console.error(error);
